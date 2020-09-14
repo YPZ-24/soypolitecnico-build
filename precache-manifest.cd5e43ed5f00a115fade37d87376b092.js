d1c3784dfb16d95233982c82e32d330040a2530d
YPZ-24 <aylinypz@gmail.com>
2020-09-14 10:37:18 -0500

    Primer Commit

---
+++ precache-manifest.cd5e43ed5f00a115fade37d87376b092.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648bd7fc81f50e5e11e1fd5ff9d505cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "958be4a426150cba95d3",
+    "url": "./static/css/main.35e8f386.chunk.css"
+  },
+  {
+    "revision": "0e47dfe66b077de3bf67",
+    "url": "./static/js/2.cbb078c6.chunk.js"
+  },
+  {
+    "revision": "96e474a8ce87a7357271f7fc52c07706",
+    "url": "./static/js/2.cbb078c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958be4a426150cba95d3",
+    "url": "./static/js/main.22d95cbb.chunk.js"
+  },
+  {
+    "revision": "5ff24408bcacadcfd080",
+    "url": "./static/js/runtime-main.def0b5b4.js"
+  },
+  {
+    "revision": "55d46b2219d88d953db948842fb76d4a",
+    "url": "./static/media/engranesBlancos.55d46b22.png"
+  },
+  {
+    "revision": "eb9d76db6d78547d20f69f2099842585",
+    "url": "./static/media/img1.eb9d76db.jpg"
+  },
+  {
+    "revision": "6ac60f1ef60449e00809ab80cfc899dc",
+    "url": "./static/media/img2.6ac60f1e.jpg"
+  },
+  {
+    "revision": "11b05de639cec450ff38ac454dbdeeed",
+    "url": "./static/media/img3.11b05de6.jpg"
+  },
+  {
+    "revision": "10fdd0ca6375b9366f93450eb5a6bbfe",
+    "url": "./static/media/tendederos.10fdd0ca.jpg"
+  }
+]);
